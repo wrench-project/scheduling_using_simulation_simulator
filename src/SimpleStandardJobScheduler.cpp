4060f0dff18e9709ec6c16b00c381284e5ba1263
Henri Casanova <henric@hawaii.edu>
2022-10-24 23:08:39 -1000

    debugging

--- src/SimpleStandardJobScheduler.cpp
+++ src/SimpleStandardJobScheduler.cpp
@@ -143,7 +143,9 @@ bool SimpleStandardJobScheduler::scheduleTask(const std::shared_ptr<wrench::Work
 //    }
 
     picked_host = "";
+    WRENCH_INFO("PICKING SERVICE %s", std::get<1>(this->enabled_scheduling_algorithms[this->current_scheduling_algorithm]).c_str());
     *picked_service = this->cluster_selection_schemes[std::get<1>(this->enabled_scheduling_algorithms[this->current_scheduling_algorithm])](task, possible_services);
+    WRENCH_INFO("PICKING NUM_CORES");
     *picked_num_cores = this->core_selection_schemes[std::get<2>(this->enabled_scheduling_algorithms[this->current_scheduling_algorithm])](task, *picked_service);
     for (auto const &entry : this->idle_cores_map[*picked_service]) {
         if (entry.second >= *picked_num_cores) {
